df2a47b1d624be35a7054ceb9fe8e2dd22279776
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-01-07 17:02:33 +0100

    refactor: remove redirect to non-existing route

--- projects/demo-app/src/app/app.routes.ts
+++ projects/demo-app/src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Route } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 export const ROUTES: Route[] = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-
   {
     path: '',
     component: LayoutComponent,
