fe610eba8e396b8bc69ae99c7aa13a81590311dc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-01-07 17:29:37 +0100

    test: cover `NgxdFormsModule`

---
+++ projects/forms/src/lib/forms.module.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormSchemaBuilder } from './forms.builder';
+import { NgxdFormsModule } from './forms.module';
+
+describe(NgxdFormsModule.name, () => {
+  describe(NgxdFormsModule.forRoot.name, () => {
+    function setup() {
+      TestBed.configureTestingModule({
+        imports: [ReactiveFormsModule, NgxdFormsModule.forRoot()],
+      });
+    }
+
+    it(`provides ${FormSchemaBuilder.name}`, () => {
+      setup();
+
+      let builder: FormSchemaBuilder;
+
+      expect(() => (builder = TestBed.inject(FormSchemaBuilder))).not.toThrow();
+      expect(builder).toBeInstanceOf(FormSchemaBuilder);
+    });
+  });
+});
