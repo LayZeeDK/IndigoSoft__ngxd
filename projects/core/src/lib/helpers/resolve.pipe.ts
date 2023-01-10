d53bddf0e1f2a9dde92a80324fdbfcbb18f21ada
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-01-10 01:31:28 +0100

    fix: support resolvers with entity class constructors in `ResolvePipe`
    This is a type-only issue but affects consumers with strict compiler settings.

--- projects/core/src/lib/helpers/resolve.pipe.ts
+++ projects/core/src/lib/helpers/resolve.pipe.ts
@@ -3,6 +3,10 @@ import { Pipe, PipeTransform, Type } from '@angular/core';
 @Pipe({ name: 'resolve', pure: true })
 export class NgxComponentOutletResolvePipe implements PipeTransform {
   transform<TEntity, TComponent>(
+    resolver: { resolve: (type: Type<TEntity>) => Type<TComponent> | null } | null,
+    value: TEntity | null
+  ): Type<TComponent> | null;
+  transform<TEntity, TComponent>(
     resolver: { resolve: (type: TEntity) => Type<TComponent> | null } | null,
     value: TEntity | null
   ): Type<TComponent> | null {
